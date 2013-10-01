f80b318f292086ed2240e1003ab894a318122a61
Derek Marcinyshyn <derek@monasheemountainmultimedia.com>
2013-09-30 23:33:44 -0700

    fix background css btn in chrome

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'fd9dd4ab1e36091fae40dc2feab78b47');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'ee255ab82adfdc250a8a6a0ac59cce04');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
